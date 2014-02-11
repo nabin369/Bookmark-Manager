325fffd09db893c87f75fa291c88679b67f4db7b
nabin369 <nabinrai369@gmail.com>
2014-02-11 17:43:16 +0000

    added adding_list_spec

--- lib/link.rb
+++ lib/link.rb
@@ -5,14 +5,10 @@ class Link
 	# this makes the instance of this class Datamapper resources
 	include DataMapper::Resource
 
+	has n, :tags, :through => Resource
 	# this block describes what resources our model will have
 	property :id, 	Serial # Serial means that it wil be auto-incremented for every record
 	property :title, String
 	property :url, String
-
-	get '/' do
-		@links = Link.all
-		erb :index
-	end
 
 end
--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
-env = ENV["RACK_ENV"] || "development"
-
-require 'data_mapper'
 require 'sinatra'
+require 'data_mapper'
+
+env = ENV["RACK_ENV"] || "development"
 
 # we're telling datamapper to use postgress database on localhost. 
 # The name will be "bookmark_manager_test" or "bookmark_manager_development" 
@@ -9,7 +9,7 @@ require 'sinatra'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialized
-
+require './lib/tag'
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -22,3 +22,18 @@ get '/' do
 	@links = Link.all
 	erb :index
 end
+
+post '/links' do
+	url = params['url']
+	title = params['title']
+		# this will either find this tag or create
+		# it if it doesn't exist already
+	tags = params['tags'].split(" ").map do |tag| 
+		Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
+
+	redirect to('/')
+end
+
+
