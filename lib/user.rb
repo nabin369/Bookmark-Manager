f22c3a9c8f792efd26282c0ba3a97646945bbd6e
nabin369 <nabinrai369@gmail.com>
2014-02-13 11:44:02 +0000

    added user signing in

--- lib/user.rb
+++ lib/user.rb
@@ -12,16 +12,30 @@ class User
 	property :email, String, :unique => true
 	# It's text and not string cause it's not enough to store hash and salt
 	property :password_digest, Text
-	# we assigned the password digest provided by bcrypt, which has hash and salt, 
-	# in the database for security reasons, instead of plain password
-	def password=(password)
-		@password = password
-		self.password_digest = BCrypt::Password.create(password)
-	end
 
 	# this is datamapper's method of validating the model, and will not be saved
 	# unless both password and password_confirmation are same.
 	validates_confirmation_of :password
 	validates_uniqueness_of :email
 
+		# we assigned the password digest provided by bcrypt, which has hash and salt, 
+	# in the database for security reasons, instead of plain password
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def self.authenticate(email, password)
+		# user trying to sign in
+		user = first(:email => email)
+
+		if user && BCrypt::Password.new(user.password_digest) == password
+			user
+		else
+			nil
+		end
+	end
+
 end
+
+
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -30,7 +30,7 @@ feature 'User signs up'  do
 	scenario "with an email that is already registered" do
 		lambda { sign_up }.should change(User, :count).by(1)
 		lambda { sign_up }.should change(User, :count).by(0)
-		expect(page).to have_content("This email is already taken")
+		expect(page).to have_content("Email is already taken")
 	end
 
 	def sign_up(email = 'alice@example.com', password = 'orange', password_confirmation = 'orange')
@@ -42,5 +42,34 @@ feature 'User signs up'  do
 		click_button 'Sign up'
 	end
 
-
 end
+
+feature 'User signs in' do
+
+	before(:each) do
+		User.create(:email => 'test@test.com', :password => 'test', :password_confirmation => 'test')
+	end
+
+	scenario 'with correct credentials' do
+		visit '/'
+		expect(page).not_to have_content('Welcome, test@test.com')
+		sign_in('test@test.com', 'test')
+		expect(page).to have_content('Welcome, test@test.com')
+	end
+
+	scenario 'with incorrect credentials' do
+		visit '/'
+		expect(page).not_to have_content('Welcome, test@test.com')
+		sign_in('test@test.com', 'wrong')
+		expect(page).not_to have_content('Welcome, test@test.com')
+	end
+
+	def sign_in(email, password)
+		visit '/sessions/new'
+		fill_in :email, :with => email
+		fill_in :password, :with => password
+		click_button 'Sign in'
+	end
+end
+
+
